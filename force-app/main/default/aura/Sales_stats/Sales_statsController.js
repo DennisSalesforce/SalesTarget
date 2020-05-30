98bb2cb0accfbf1630c5b42141130484d9ca92fc
Herman Dempere <herman.dempere@emark.com>
2020-05-30 11:15:31 +0200

    Latest changes

--- force-app/main/default/aura/Sales_stats/Sales_statsController.js
+++ force-app/main/default/aura/Sales_stats/Sales_statsController.js
@@ -4,36 +4,41 @@
 		helper.doInitDaysLeft(component, event);
 		helper.showAwards(component, event);
 		helper.getAwards(component, event);
-		helper.showHotOpportunityHelper(component, event);
+		helper.getHotOpportunities(component, event);
 	},
 
     loadProgress : function(component,event,helper){
     	helper.onRender(component, event);
 	},
 
-	onClickGold : function(component,event,helper){	
-		helper.showWonAwardsGoldHelper(component, event); 
+	onClickGold : function(component,event,helper){
+		helper.showWonAwardsGoldHelper(component, event);
 	},
-	onClickSilver : function(component,event,helper){	
-		helper.showWonAwardsSilverHelper(component, event); 
+	onClickSilver : function(component,event,helper){
+		helper.showWonAwardsSilverHelper(component, event);
 	},
-	onClickBronze : function(component,event,helper){	
-		helper.showWonAwardsBronzeHelper(component, event); 
+	onClickBronze : function(component,event,helper){
+		helper.showWonAwardsBronzeHelper(component, event);
 	},
-	onClickHundred : function(component,event,helper){	
-		helper.showWonAwardsHundredHelper(component, event); 
-	},
-
-	onClickClose : function(component,event,helper){	
-		helper.closeShowWonAwardsHelper(component, event); 
-	},
-	
-	onClickShowOpps : function(component,event,helper){	
-		helper.getWonWonOppsHelper(component, event); 
+	onClickHundred : function(component,event,helper){
+		helper.showWonAwardsHundredHelper(component, event);
 	},
 
-	closeShowOpps : function(component,event,helper){	
-		helper.closeWonWonOppsHelper(component, event); 
+	onClickClose : function(component,event,helper){
+		helper.closeShowWonAwardsHelper(component, event);
+	},
+
+	toggleHotOpportunities : function(component, event, helper){
+		var currentValue = component.get('v.showHotOpportunties')
+		component.set('v.showHotOpportunties', !currentValue);
+	},
+
+	onClickShowOpps : function(component,event,helper){
+		helper.getWonWonOppsHelper(component, event);
+	},
+
+	closeShowOpps : function(component,event,helper){
+		helper.closeWonWonOppsHelper(component, event);
 	}
 
 })
